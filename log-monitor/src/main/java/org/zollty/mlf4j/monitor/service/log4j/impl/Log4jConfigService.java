b54844f58cca5ab9bc6aa46675d773e109f742ee
Zollty Tsou <zollty@163.com>
2017-05-03 15:20:39 +0800

    Update Log4jConfigService.java

--- log-monitor/src/main/java/org/zollty/mlf4j/monitor/service/log4j/impl/Log4jConfigService.java
+++ log-monitor/src/main/java/org/zollty/mlf4j/monitor/service/log4j/impl/Log4jConfigService.java
@@ -1,6 +1,4 @@
 /* 
- * Copyright (C) 2012-2014 TravelSky Technology Limited.
- * 
  * == Mlf4j (Monitoring Logging Facade for Java) ==
  * ============为监控而生的通用日志工具库===========
  * 
@@ -11,7 +9,6 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * Create by Zollty Tsou (zouty@travelsky.com, http://blog.csdn.net/zollty)
  */
 package org.zollty.mlf4j.monitor.service.log4j.impl;
 
